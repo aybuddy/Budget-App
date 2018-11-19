67846f223549832f5c347e5ac5ead1227f95e04c
Aaron Yu <ayybuddyy@gmail.com>
2018-11-19 00:21:10 -0500

    Error Handling for input values
    Change the input value for data object from string to integer. Add
    logic to check if input fields are empty, 0 or NaN.

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ var UIController = (function() {
             return {
                 type : document.querySelector(DOMstrings.inputType).value,
                 description : document.querySelector(DOMstrings.inputDescription).value,
-                value : document.querySelector(DOMstrings.inputValue).value
+                value : parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
             // will be either inc or exp
         },
@@ -133,19 +133,29 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
     }
 
+    var updateBudget = function() {
+        // 1. Calculate the budget
+        // 2. Return the budget
+        // 3. Display the budget on the UI
+    }
+
     var ctrlAddItem = function() {
         var input, newItem;
 
         // 1. Get the field input data
         input = UICtrl.getInput();
-        // 2. Add item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-        // 3. Add the new item to the UI
-        UICtrl.addListItem(newItem, input.type);
-        // 4. clear the fields
-        UICtrl.clearFields();
-        // 4. Calculate the budget
-        // 5. Display the budget on the UI
+
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+            // 3. Add the new item to the UI
+            UICtrl.addListItem(newItem, input.type);
+            // 4. clear the fields
+            UICtrl.clearFields();
+            // 5. Calculate and update the budget
+            updateBudget();
+        }
+        
     };
     
     return {
