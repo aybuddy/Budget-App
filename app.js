0cf9640742a76347581d63110b8f89de8c1fcdca
Aaron Yu <ayybuddyy@gmail.com>
2018-11-18 22:21:55 -0500

    Set up basic structure, add eventlisteners
    MVC pattern with budget controller, UI controller, and Global app
    controller. Object created to hold DOM class elements. Returns input
    function to get inputs from UI, makes DOMstrings variable public.
    Global controller initializes eventlisteners. Add UI function from
    input. Init function is called to start app with event listeners
    inside function.

---
+++ app.js
@@ -0,0 +1,72 @@
+// Budget Controller
+var budgetController = (function() {
+    
+    
+
+})();
+
+
+// UI Controller
+var UIController = (function() {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    }
+
+    return {
+        getInput: function() {
+            return {
+                type : document.querySelector(DOMstrings.inputType).value,
+                description : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
+            };
+            // will be either inc or exp
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+})();
+
+
+// Global App Controller
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            if(event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    }
+
+
+
+    var ctrlAddItem = function() {
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+        // 2. Add item to the budget controller
+        // 3. Add the new item to the UI
+        // 4. Calculate the budget
+        // 5. Display the budget on the UI
+    };
+    
+    return {
+        init: function() {
+            console.log("App has started");
+            setupEventListeners();
+        }
+    };
+   
+})(budgetController, UIController);
+
+controller.init();
+
