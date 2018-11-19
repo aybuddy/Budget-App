c4f3264caaec2bb823b7bc8b4376a3c687ef138a
Aaron Yu <ayybuddyy@gmail.com>
2018-11-18 23:55:28 -0500

    Add function to add input to UI
    function takes in 2 parameters, object and type and adds the input
    from the UI to the data object holding the inc and exp. Adds HTML
    syntax by updating the DOM with insertAdjacentHTML string method.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Budget Controller
 var budgetController = (function() {
-    
+
     var Expense = function(id, description, value) {
         this.id = id;
         this.description = description;
@@ -63,7 +63,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputButton: '.add__btn'
+        inputButton: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
 
     return {
@@ -74,6 +76,25 @@ var UIController = (function() {
                 value : document.querySelector(DOMstrings.inputValue).value
             };
             // will be either inc or exp
+        },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+            // create HTML string with placeholder text
+
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }            
+            // replace the placeholder text with data received from object
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            // insert HTML into DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
         getDOMstrings: function() {
@@ -98,8 +119,6 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
     }
 
-
-
     var ctrlAddItem = function() {
         var input, newItem;
 
@@ -108,6 +127,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 2. Add item to the budget controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the new item to the UI
+        UICtrl.addListItem(newItem, input.type);
         // 4. Calculate the budget
         // 5. Display the budget on the UI
     };
