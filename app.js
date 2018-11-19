709486d8fa53f26fbab16341d6322ae61acc24e2
Aaron Yu <ayybuddyy@gmail.com>
2018-11-19 00:07:36 -0500

    Add function to clear the input field
    Add clearFields function in the UI controller to clear the fields
    after user puts input. Focus is back at description after user puts
    in input.

--- app.js
+++ app.js
@@ -97,6 +97,20 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -128,6 +142,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the new item to the UI
         UICtrl.addListItem(newItem, input.type);
+        // 4. clear the fields
+        UICtrl.clearFields();
         // 4. Calculate the budget
         // 5. Display the budget on the UI
     };
